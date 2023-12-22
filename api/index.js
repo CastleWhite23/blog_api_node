42212a3868f7d42ce44716d5a226b296f3d5671c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2023-12-22 11:18:16 -0300

    Configurando corretamente o index da api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,22 @@
-const { error, Console } = require('console');
-const { express } = require('express');
-const app = express();
 
+const express  = require("express");
+const app = express();
 const port = 8081;
 const criarTabela = require('./inc/tabelas')
 const connection = require('./inc/connection')
+
 const router = require('./routes/index');
-const { route } = require('./routes/routerPostagem');
+
 
 criarTabela.init(connection)
 router(app, express)
 
 
-app.listen(port, (error)=>{
+app.listen(port, (error) => {
     if(error){
-        console.log('Deu erro', error)
+        console.log(error);
+        return;
     }
-
-    console.log("Api rodando em localhost:", port)
-})
+    console.log(`A porta acessada é: ${port}`);
+    console.log(`http://localhost:${port}/postagens`);
+});
