d3cc8cab33d7560d0152bf7c053237d36b21ae11
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 16:21:49 -0300

    adicionando componente mini slider

---
+++ app/src/components/MiniSlider/MiniSlider.jsx
@@ -0,0 +1,18 @@
+const MiniSlider = () => {
+
+    return (
+        <><div className="layout">
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+            <div className="mini-post"></div>
+        </div>
+        </>
+    )
+
+}
+
+export default MiniSlider
