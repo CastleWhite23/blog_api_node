63f5ec4b48b620e30e5ad1fd9da305ef26627ad3
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 15:48:54 -0300

    adicioando home em uma pasta separada

---
+++ app/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import BlogLanding  from "../../components/BlogLanding/BlogLanding"
+
+const Home = () =>{
+    return (
+        <>
+            <BlogLanding />
+        </>
+    )
+}
+
+export default Home
