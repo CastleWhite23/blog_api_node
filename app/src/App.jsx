92b3a064a505ccb60a14984e41586ccc3d65a230
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 15:49:22 -0300

    removendo estilo do footer do style

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 import Layout from './components/Layout/Layout'
 import Blogs from './components/Blogs/Blogs';
 import NovoPost from './components/NovoPost/NovoPost';
