0d694f6e72af048ba3bc8f981f84cca352593ecb
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 16:04:26 -0300

    Adicionando component slider

---
+++ app/src/components/Slider/Slider.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import './style.css'
+
+
+const Slider = () => {
+    return(
+        <>
+             <div className='slider'>
+                    oi
+             </div>
+        </>
+    )
+}
+
+
+export default Slider
