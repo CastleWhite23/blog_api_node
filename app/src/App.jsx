34ba0a7d1d1fcd7af71917f72a7baefe1154d23a
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-03 15:25:10 -0300

    importando estilo geral no app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Layout from './components/Layout/Layout'
 import Blogs from './components/Blogs/Blogs';
 import NovoPost from './components/NovoPost/NovoPost';
+import './components/style.css'
 function App() {
   //aqui tera os components dos links do reac-router-dom
   //aqui tera os components e o app context
