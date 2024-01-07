c64d3fc3f1c90ed27aba353f326ddf7c86815a2b
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:13:20 -0300

    usando rota de usuarios no index das rotas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const routerPostagem = require("./routerPostagem")
+const routerUsuario = require("./routerUsuario")
 
 const router = (app, express) =>{
     app.use(express.json())
     app.use(express.urlencoded({ extended: true}))
     app.use(routerPostagem)
+    app.use(routerUsuario)
 }
 
 module.exports = router;
