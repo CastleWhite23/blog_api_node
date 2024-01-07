493d4a624c72d7155eb7af2c8d3876dcd8f798ee
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:32:25 -0300

    adicionando metodo processar promise na classe de usuario controller

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -1,4 +1,14 @@
 class UsuarioController{
+    async processarPromise(promise, status, error, res) {
+        try{
+            const response = await promise;
+            res.status(status)
+            res.json(response)
+        }catch{
+            res.status(error)
+        }   
+    }
+
     login(req, res){
 
     }
