3c94c752316c2a1fad00fb03672e3f90379c1563
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:42:30 -0300

    fazendo a chamada a func executarWuery no modelsPost

--- api/models/PostModels.js
+++ api/models/PostModels.js
@@ -1,42 +1,29 @@
-const { error } = require("console")
-const connection = require("../inc/connection")
-const { resolve } = require("path")
-const { rejects } = require("assert")
 
+const connection = require("../inc/connection")
+const executarQuery  = require("../utilites/executarQuery")
 
 class PostModel {
 
-    executarQeury(sql, params = "") {
-       return new Promise((resolve, rejects)=>{
-            connection.query(sql, params, (error, resposta)=>{
-                if(error){
-                    rejects(error)
-                    return;
-                }
-
-                resolve(resposta)
-            })
-       })
-    }
+  
 
     buscar() {
         const sql = `SELECT * FROM post`
-        return this.executarQeury(sql);
+        return executarQeury(sql);
     }
 
     buscarById(id) {
         const sql = `SELECT * FROM post WHERE id_post = '${id}'`
-        return this.executarQeury(sql)
+        return executarQeury(sql)
     }
 
     criar(params) {
         const sql = `INSERT INTO \`post\` SET ?`
-        return this.executarQeury(sql, params)
+        return executarQeury(sql, params)
     }
 
     atualizar(params, id) {
         const sql = `UPDATE post SET ? WHERE id_post = ?`
-        return this.executarQeury(sql, [params, id])
+        return executarQeury(sql, [params, id])
     }
 
     deletar(id) {
