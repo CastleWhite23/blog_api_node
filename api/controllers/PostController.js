97fe0b2268dd8dc8c221ccdd58005813f2d5e0d9
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:38:58 -0300

    importando func processar promise no controller dos posts

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -1,48 +1,37 @@
 
 const { response } = require('express');
 const postModel = require('../models/PostModels');
+import { processarPromise } from '../utilities/processarPromise';
 const { error } = require('console');
 
 class PostController {
 
-    async processarPromise(promise, status, error, res) {
-        try{
-            const response = await promise;
-            res.status(status)
-            res.json(response)
-        }catch{
-            res.status(error)
-        }   
-    }
-
     buscar(req, res) {
         const resPostModel = postModel.buscar()
-        this.processarPromise(resPostModel, 200, 400, res)
+        processarPromise(resPostModel, 200, 400, res)
     }
 
     buscarById(req, res, id) {
         const resPostModel = postModel.buscarById(id);
-        this.processarPromise(resPostModel, 200, 400, res)
+        processarPromise(resPostModel, 200, 400, res)
        
     }
 
     criar(res, req, params) {
         const resPostModel = postModel.criar(params)
-        this.processarPromise(resPostModel, 201, 400, res)
+        processarPromise(resPostModel, 201, 400, res)
     }
 
     atualizar(req, res, params, id) {
         const resPostModel = postModel.atualizar(params, id)
 
-        this.processarPromise(resPostModel, 200, 400, res)
+        processarPromise(resPostModel, 200, 400, res)
     }
 
     deletar(req, res, id) {
         const resPostModel = postModel.deletar(id)
-        this.processarPromise(resPostModel, 200, 400, res)
+        processarPromise(resPostModel, 200, 400, res)
     }
-
-
 }
 
 
