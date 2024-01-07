cc68974e58f3aedbb31450856b8438389dfd11a9
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 16:04:33 -0300

    adicioando metodo para criar tabela usuarios no bd se nao existir

--- api/inc/tabelas.js
+++ api/inc/tabelas.js
@@ -2,10 +2,11 @@ class Tabelas {
     init(connection) {
         this.connection = connection
         this.createTabelaPostagem()
+        this.createTableUsuario()
     }
 
     createTabelaPostagem() {
-    const sql = `
+        const sql = `
         CREATE TABLE  IF NOT EXISTS \`post\` (
             \`id_post\` INT PRIMARY KEY AUTO_INCREMENT,
             \`titulo_post\` VARCHAR(30),
@@ -15,17 +16,35 @@ class Tabelas {
             \`data_alteracao\` DATETIME
         );`
 
-    this.connection.query(sql, (error) => {
-        if (error) {
-            console.log(error)
-            return;
-        }
+        this.connection.query(sql, (error) => {
+            if (error) {
+                console.log(error)
+                return;
+            }
 
-        console.log("tabela criada com sucesso");
-    })
+            console.log("tabela criada com sucesso");
+        })
+    }
+
+    createTableUsuario() {
+        const sql = `CREATE TABLE IF NOT EXISTS usuario(
+        id_user INT PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(30),
+        username VARCHAR(30),
+        email VARCHAR(60),
+        password VARCHAR(150)
+    );`
+
+        this.connection.query(sql, (error) => {
+            if (error) {
+                console.log(error)
+                return;
+            }
+
+            console.log("tabela criada com sucesso");
+        })
+    }
 }
 
 
-}
-
-module.exports  = new Tabelas()
+module.exports = new Tabelas()
