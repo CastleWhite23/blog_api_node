702a6c102724019b7623bf957806bf006912d57c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 16:06:15 -0300

    importando a func executar query e usando ela

--- api/models/PostModels.js
+++ api/models/PostModels.js
@@ -1,35 +1,32 @@
-
-const connection = require("../inc/connection")
-const executarQuery  = require("../utilites/executarQuery")
-
+const executarQuery  = require("../utilities/executarQuery")
 class PostModel {
 
   
 
     buscar() {
         const sql = `SELECT * FROM post`
-        return executarQeury(sql);
+        return executarQuery(sql);
     }
 
     buscarById(id) {
         const sql = `SELECT * FROM post WHERE id_post = '${id}'`
-        return executarQeury(sql)
+        return executarQuery(sql)
     }
 
     criar(params) {
         const sql = `INSERT INTO \`post\` SET ?`
-        return executarQeury(sql, params)
+        return executarQuery(sql, params)
     }
 
     atualizar(params, id) {
         const sql = `UPDATE post SET ? WHERE id_post = ?`
-        return executarQeury(sql, [params, id])
+        return executarQuery(sql, [params, id])
     }
 
     deletar(id) {
         const sql = `DELETE FROM post WHERE id_post = ?`
 
-        return this.executarQeury(sql, id)
+        return this.executarQuery(sql, id)
     }
 
 
