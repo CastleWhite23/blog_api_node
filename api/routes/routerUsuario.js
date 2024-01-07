d8f37dd18bf308def1c855d346469c9dd2f8df15
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:09:45 -0300

    crinado router dos usuarios

---
+++ api/routes/routerUsuario.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+const router = Router();
+
+
+router.get("/contas", (req, res) =>{
+
+})
+
+router.post("/conta/login", (req, res) =>{
+
+})
+
+router.post("/conta/cadastro", (req, res) =>{
+
+})
+
+
+router.put("/conta/atualizar", (req, res) =>{
+    
+})
+
+
+router.delete("/conta/excluir", (req, res) =>{
+    
+})
+
+
+module.exports  = router
