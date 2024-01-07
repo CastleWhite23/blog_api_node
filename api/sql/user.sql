b33fdd0e896676ff71f35756aea8b9b288930d77
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 16:04:13 -0300

    corrigindo erros de criacao na tabel usuario e salvando no arquivo user

--- api/sql/user.sql
+++ api/sql/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE usuario(
     `id_user` INT PRIMARY KEY AUTO_INCREMENT,
-    `nome` VARCHAR(30)
+    `nome` VARCHAR(30),
     `username` VARCHAR(30),
     `email` VARCHAR(60),
-    `password` VARCHAR(150),
+    `password` VARCHAR(150)
 );
