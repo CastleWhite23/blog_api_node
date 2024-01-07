eee153e0fd37063bbb65981aaa7cf13a9b207be7
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 16:05:57 -0300

    importando a func executar query e utilizando ela

--- api/models/usuarioModel.js
+++ api/models/usuarioModel.js
@@ -1,41 +1,30 @@
+const executarQuery = require("../utilities/executarQuery")
 class UsuarioModel {
-    executarQeury(sql, params = "") {
-        return new Promise((resolve, rejects)=>{
-             connection.query(sql, params, (error, resposta)=>{
-                 if(error){
-                     rejects(error)
-                     return;
-                 }
- 
-                 resolve(resposta)
-             })
-        })
-     }
- 
+
      buscarCadastros() {
-         const sql = `SELECT * FROM usuarios`
-         return this.executarQeury(sql);
+         const sql = `SELECT * FROM usuario`
+         return executarQuery(sql);
      }
  
      buscarById(id) {
          const sql = `SELECT * FROM usuario WHERE id_user = '${id}'`
-         return this.executarQeury(sql)
+         return executarQuery(sql)
      }
  
      cadastrar(params) {
          const sql = `INSERT INTO \`usuario\` SET ?`
-         return this.executarQeury(sql, params)
+         return executarQuery(sql, params)
      }
  
      atualizarCadastrp(params, id) {
          const sql = `UPDATE usuario SET ? WHERE id_user = ?`
-         return this.executarQeury(sql, [params, id])
+         return executarQuery(sql, [params, id])
      }
  
      excluirCadastro(id) {
          const sql = `DELETE FROM usuario WHERE id_post = ?`
 
-         return this.executarQeury(sql, id)
+         return executarQuery(sql, id)
      }
  
  
