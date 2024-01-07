8c860d6a212bce568f64d33cc710fcd36cdff1ee
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 15:20:35 -0300

    criando class do controller de usuarios

---
+++ api/controllers/usuarioController.js
@@ -0,0 +1,24 @@
+class UsuarioController{
+    login(){
+
+    }
+
+    todosCadastros(){
+
+    }
+
+    cadastro(){
+
+    }
+
+    atualizarCadastro(){
+
+    }
+
+    excluirCadastro(){
+
+    }
+
+}
+
+module.exports =  new UsuarioController()
