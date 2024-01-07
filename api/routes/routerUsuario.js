fc8b1c4c1db5727cf000021a22a8b869968f3eb1
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-07 16:05:33 -0300

    atualizando router usuario em vez de usar a desconstrucao do objeto usar require

--- api/routes/routerUsuario.js
+++ api/routes/routerUsuario.js
@@ -1,18 +1,14 @@
-import { Router } from "express";
-const usuarioController = require("../controllers/usuarioController");
-const router = Router();
 
+ const usuarioController = require("../controllers/usuarioController");
 
+const express = require("express");
+const router = express.Router();
 
-router.get("/contas", usuarioController.buscarCadastros)
+router.get("/contas", usuarioController.buscarCadastros);
+router.post("/conta/login", usuarioController.login);
+router.post("/conta/cadastro", usuarioController.cadastro);
+router.put("/conta/atualizar", usuarioController.atualizarCadastro);
+router.delete("/conta/excluir", usuarioController.excluirCadastro);
 
-router.post("/conta/login",usuarioController.login)
+module.exports = router;
 
-router.post("/conta/cadastro", usuarioController.cadastro)
-
-router.put("/conta/atualizar", usuarioController.atualizarCadastro)
-
-router.delete("/conta/excluir", usuarioController.excluirCadastro)
-
-
-module.exports = router
