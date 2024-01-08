6a03a195a32090ba0dca3cae926c2322af265e6f
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 10:09:53 -0300

    Renomeando arquivo de autenticacao do controller

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,9 @@
+class AuthController{
+       
+    login(req, res){
+        //o objetiuvo dessa rota é se o usuario for o correto e as infos baterem entao gerar um token e deixa-lo no cookie do navegador
+    }
+}
+
+
+module.exports =  new AuthController()
