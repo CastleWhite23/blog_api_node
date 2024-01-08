8cb1190bdb0d2526d4c324561c55507338c607fb
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 11:56:10 -0300

    passando outro parametro para a func de  gerador de token

--- api/models/AuthModel.js
+++ api/models/AuthModel.js
@@ -5,17 +5,15 @@ const jwt = require('jsonwebtoken');
 
 
 class AuthModel{
-        login(username){
+        login(req, username){
            
             //responsavel por gerar o token jwt e colocar nos cookies
             try{
                 const token =  jwt.sign({username: username}, process.env.SECRET)
-                localStorage.setItem('token', token)
                 return {message: 'token gerado', token: token, auth: true}
             }catch(e){
-                if(e){
-                    return {message: 'Erro na geracao de token', token: "", auth: false}
-                }
+                console.log(e)
+                return {message: 'Erro na geracao de token', token: "", auth: false}
             }
             // jwt.verify(token, process.env.SECRET, (err, decoded) =>{
             //     if (err) {
