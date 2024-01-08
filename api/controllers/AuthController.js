0fe69f4bf3d3a0fcac6f6f4d9120a2172c52dc57
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 11:30:17 -0300

    criando func e verifcacoes para gerar o token

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,7 +1,23 @@
+const usuarioModel = require("../models/usuarioModel")
+const authModel = require("../models/AuthModel")
+
 class AuthController{
        
-    login(req, res){
-        //o objetiuvo dessa rota é se o usuario for o correto e as infos baterem entao gerar um token e deixa-lo no cookie do navegador
+     async login(req, res){
+        //o objetivo dessa rota é se o usuario for o correto e as infos baterem entao gerar um token e deixa-lo no cookie do navegador
+        const { username, senha } = req.body
+        const response = await usuarioModel.verificaConta(username, senha)
+
+        if(response){
+            const realizarLogin = authModel.login(response[0].username)
+            res.send(JSON.stringify(realizarLogin))
+        }
+
+        res.send(JSON.stringify({message: "token nao gerado"}))
+        //verificar os dados da conta
+        //se for true func para gerar token e colocar nos cookies do navegador
+
+
     }
 }
 
