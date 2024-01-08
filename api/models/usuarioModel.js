56c400f53a1f657bfc27831409a95d0ebf470816
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 11:30:58 -0300

    criando funcao por verificar se o usuario existe no banco de dados  para gerar o token

--- api/models/usuarioModel.js
+++ api/models/usuarioModel.js
@@ -1,33 +1,53 @@
+const { error } = require("console")
 const executarQuery = require("../utilities/executarQuery")
+const e = require("express")
 class UsuarioModel {
 
-     buscarCadastros() {
-         const sql = `SELECT * FROM usuario`
-         return executarQuery(sql);
-     }
- 
-     buscarById(id) {
-         const sql = `SELECT * FROM usuario WHERE id_user = '${id}'`
-         return executarQuery(sql)
-     }
- 
-     cadastrar(params) {
-         const sql = `INSERT INTO \`usuario\` SET ?`
-         return executarQuery(sql, params)
-     }
- 
-     atualizarCadastrp(params, id) {
-         const sql = `UPDATE usuario SET ? WHERE id_user = ?`
-         return executarQuery(sql, [params, id])
-     }
- 
-     excluirCadastro(id) {
-         const sql = `DELETE FROM usuario WHERE id_user = ?`
+    async verificaConta(username, senha) {
+        //criptografia da senha
+        const sql = `SELECT * FROM usuario WHERE username = ? AND password = ?`
+            const response = await executarQuery(sql, [username, senha])
+            //verificar se exisite um usuario com aqueles dados
+            if (!response.data) {
+                return response
+            }
 
-         return executarQuery(sql, id)
-     }
- 
- 
+            return response
+
+
+
+
+    }
+
+
+
+    buscarCadastros() {
+        const sql = `SELECT * FROM usuario`
+        return executarQuery(sql);
+    }
+
+    buscarById(id) {
+        const sql = `SELECT * FROM usuario WHERE id_user = '${id}'`
+        return executarQuery(sql)
+    }
+
+    cadastrar(params) {
+        const sql = `INSERT INTO \`usuario\` SET ?`
+        return executarQuery(sql, params)
+    }
+
+    atualizarCadastrp(params, id) {
+        const sql = `UPDATE usuario SET ? WHERE id_user = ?`
+        return executarQuery(sql, [params, id])
+    }
+
+    excluirCadastro(id) {
+        const sql = `DELETE FROM usuario WHERE id_user = ?`
+
+        return executarQuery(sql, id)
+    }
+
+
 
 }
 
