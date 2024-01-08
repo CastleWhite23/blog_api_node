ee149dfc3238018f75544795c90be548cc3f233e
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 11:54:57 -0300

    adicionando midleware

--- api/routes/routerUsuario.js
+++ api/routes/routerUsuario.js
@@ -1,11 +1,12 @@
 
 const authController = require("../controllers/AuthController");
 const usuarioController = require("../controllers/usuarioController");
+const verificaToken =  require("../middleware/auth")
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/contas", usuarioController.buscarCadastros);
+router.get("/contas", verificaToken, usuarioController.buscarCadastros);
 router.post("/conta/login", authController.login);
 router.post("/conta/cadastro", usuarioController.cadastro);
 router.put("/conta/atualizar/:id", usuarioController.atualizarCadastro);
