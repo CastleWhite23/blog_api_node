2c3fe8f9d359541bd7f3a61064977de249527524
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-08 10:10:43 -0300

    adicioando controller na rota de login

--- api/routes/routerUsuario.js
+++ api/routes/routerUsuario.js
@@ -1,11 +1,12 @@
 
- const usuarioController = require("../controllers/usuarioController");
+const authController = require("../controllers/AuthController");
+const usuarioController = require("../controllers/usuarioController");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/contas", usuarioController.buscarCadastros);
-router.post("/conta/login", usuarioController.login);
+router.post("/conta/login", authController.login);
 router.post("/conta/cadastro", usuarioController.cadastro);
 router.put("/conta/atualizar/:id", usuarioController.atualizarCadastro);
 router.delete("/conta/excluir/:id", usuarioController.excluirCadastro);
