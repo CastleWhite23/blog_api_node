ec91a89a9ff0d91d9fc094692434dafebe4f6bca
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-09 14:02:58 -0300

    Retirando todas as rotas no app e chamando o componente main routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import Home from './pages/Home/Home'
-import BlogPage from './pages/BlogPage/BlogPage'
-import NovoPostPage from './pages/NovoPostPage/NovoPostPage'
-import CadUsuarioPage from './pages/CadUsuarioPage/CadUsuarioPage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import EditarPostPage from './pages/EditarPostPage/EditarPostPage'
-import ExcluirPostPage from './pages/ExcluirPostPage/ExcluirPostPage'
 
+import MainRoutes from '../components/MainRoutes/MainRoutes'
 
 
 import Layout from './components/Layout/Layout'
@@ -24,21 +18,7 @@ function App() {
       <BrowserRouter>
         <ChakraProvider>
           <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* Rotas referentes aos post */}
-
-              <Route path="/postagens" element={<BlogPage />} />
-              <Route path="/postagens/novo" element={<NovoPostPage />} />
-              <Route path="/postagens/editar/:key" element={<EditarPostPage />} />
-              <Route path="/postagens/excluir/:key" element={<ExcluirPostPage />} />
-
-              {/* Rotas referentes ao usuario */}
-              <Route path="/usuarios/cadastro" element={<CadUsuarioPage />} />
-              <Route path="/usuarios/login" element={<LoginPage />} />
-              
-              {/* no home vai ter o get started com uma landing page, é pra ter uma pg pros posts */}
-            </Routes>
+            <MainRoutes/>
           </Layout>
         </ChakraProvider>
       </BrowserRouter>
