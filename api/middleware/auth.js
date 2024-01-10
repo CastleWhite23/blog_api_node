e3c54fbe6053fc1c9142fce058993e9114c34b10
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-10 10:02:19 -0300

    pegando o token do header

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -2,18 +2,22 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const verificaToken = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'] || req.cookies.token;
+    const tokenHeader = req.headers['authorization']
+    const token = tokenHeader && tokenHeader.split(" ")[1];
 
-    if (!token) return res.status(401).json({ message: 'Precisa realizar o login antes.' });
 
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        if (err) {
-            return res.staus(500).json({ message: "Falha na autenticacao" })
-        }
-        req.username = decoded.username;
-        next()
-    })
+    if (!token) return res.status(401).json({ message: 'Não autorizado' });
 
+    try{
+        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+            req.username = decoded.username;
+            next()
+        })
+    
+    }catch{
+        return res.staus(500).json({ message: "Falha na autenticacao" })
+    }
+   
 
 }
 
