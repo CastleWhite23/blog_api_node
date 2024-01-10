5b8153e7864b89e93f81beb26cf7591539bba9ff
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-10 10:19:01 -0300

    criando componente global

--- app/src/components/AppContext/AppContext.jsx
+++ app/src/components/AppContext/AppContext.jsx
@@ -1,9 +1,19 @@
-const AppContext = () =>{
+import { createContext, useState } from "react"
+
+
+
+export const AppContext = createContext()
+
+const AppContextProvider = ( { children } ) =>{
+    const [tokenAuth, setTokenAuth] = useState("")
+
     return(
         <>
-            
+            <AppContext.Provider value={ { tokenAuth, setTokenAuth }}>
+                { children }
+            </AppContext.Provider>
         </>
     )
 }
 
-export default AppContext
+export default AppContextProvider
