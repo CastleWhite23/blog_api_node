13c1a103e1b2c1ef1ea3648815e305efcdf65be8
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-10 09:38:58 -0300

    retirando comentarios

--- api/models/AuthModel.js
+++ api/models/AuthModel.js
@@ -6,16 +6,13 @@ const jwt = require('jsonwebtoken');
 
 class AuthModel{
         login(username){
-            //responsavel por gerar o token jwt e colocar nos cookies
             try{
                 const token =  jwt.sign({username: username}, process.env.SECRET)
-                //  res.cookie('token', token, { httpOnly: true})
                 return {message: 'token gerado', auth: true, token: token}
             }catch(e){
                 console.log(e)
                 return {message: 'Não foi possivel gerar token',  auth: false, token: ""}
             }
-            
         }
 }
 
