a2b4c1aad984cb7604b224fb75d3a8e5196f323d
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-11 10:22:59 -0300

    criando useEffect para pegar o cookie do navegador

--- app/src/components/AppContext/AppContext.jsx
+++ app/src/components/AppContext/AppContext.jsx
@@ -1,16 +1,19 @@
-import { createContext, useState } from "react"
-
+import { createContext, useEffect, useState } from "react"
+import { getCookie } from '../../services/funcCookies'
 
 
 export const AppContext = createContext()
 
-export const AppContextProvider = ( { children } ) =>{
+export const AppContextProvider = ({ children }) => {
     const [tokenAuth, setTokenAuth] = useState("")
+    useEffect(() => {
+        setTokenAuth(getCookie('token'))
+    }, [])
 
-    return(
+    return (
         <>
-            <AppContext.Provider value={ { tokenAuth, setTokenAuth }}>
-                { children }
+            <AppContext.Provider value={{ tokenAuth, setTokenAuth }}>
+                {children}
             </AppContext.Provider>
         </>
     )
