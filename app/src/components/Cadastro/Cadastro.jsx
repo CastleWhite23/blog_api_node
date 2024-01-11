46898c910a294d439fe9b2492a4f4f5fa9081e8c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-11 09:09:19 -0300

    Criando componente de cadastro de usuario

---
+++ app/src/components/Cadastro/Cadastro.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-undef */
+import { Textarea, Input, Center, Button } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+import './style.css'
+import { api } from "../../services/api"
+import { useState } from 'react'
+import { getData } from '../../services/getData'
+
+
+const Cadastro = () => {
+    const  navigate =  useNavigate()
+    const [titulo, setTitulo] = useState("")
+    const [historia, setHistoria] = useState("")
+    const autor = "Pedro"
+    const data_criacao = getData();
+    
+
+    const handleCriarPostClick = () => {
+        createPost(titulo, historia, autor, data_criacao)
+        navigate("/postagens")
+    };
+
+    const createPost = (titulo, historia, autor, data_criacao) => {
+        api.post('/postagens/novo', {
+            titulo_post: titulo,
+            conteudo_post: historia,
+            autor_post: autor,
+            data_criacao: data_criacao,
+            data_alteracao: data_criacao
+        })
+    }
+
+
+    return (
+        <>
+            <form>
+                <Center flexDirection={"column"} rowGap={10}>
+                    <h1>Compartilhe sua história</h1>
+                    <Input
+                        variant='flushed'
+                        placeholder='Digite o titulo'
+                        fontSize={20}
+                        value={titulo}
+                        onChange={(e) => {
+                            setTitulo(e.target.value)
+                        }}
+                    />
+                    <Textarea
+                        height={10}
+                        value={historia}
+                        onChange={(e) => {
+                            setHistoria(e.target.value)
+                        }}
+                    />
+                    <Button
+                        backgroundColor={"#303030"}
+                        color={"#fff"}
+                        size='lg'
+                        onClick={handleCriarPostClick}
+                    >
+                        Compartilhar
+                    </Button>
+                </Center>
+            </form>
+        </>
+    )
+}
+
+
+export default Cadastro
