0238d369631771291b8c3afbdda69eea1c074993
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-12 13:27:27 -0300

    criando verificções de login para ter btn delogar

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,36 @@
 /* eslint-disable no-undef */
 import { Link } from "react-router-dom"
+import { useContext } from "react"
 import './style.css'
+import { AppContext } from "../AppContext/AppContext"
+import { deleteCookie } from "../../services/funcCookies"
 
 
 const Navbar = () => {
-    return(
+    const { tokenAuth, setTokenAuth } = useContext(AppContext)
+    const handleClickDeslogar = () =>{
+        deleteCookie('token')
+        setTokenAuth("")
+    }
+
+    return (
         <>
-             <nav>
+            <nav>
                 <ul>
-                    <li><Link  to="/">Home</Link></li>
-                    <li><Link  to="/postagens">Histórias</Link></li>
-                    <li><Link  to="/postagens/novo">novo post</Link></li>
-                    <li><Link  to="/usuarios/login">Login</Link></li>
-                    <li><Link  to="/usuarios/cadastro">Cadastro</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/postagens">Histórias</Link></li>
+                    <li><Link to="/postagens/novo">novo post</Link></li>
+                    {
+                        !tokenAuth ? (
+                            <>
+                                <li><Link to="/usuarios/login">Login</Link></li>
+                                <li><Link to="/usuarios/cadastro">Cadastro</Link></li>
+                            </>
+                        ) : (
+                            <li onClick={handleClickDeslogar}>Deslogar</li>
+                        )
+                    }
+
                 </ul>
             </nav>
         </>
