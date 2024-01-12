9e13a2daaeab25a855577e0ca7eeec2bd16d53b6
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-12 12:52:58 -0300

    Importando conta usuario Page

--- app/src/components/MainRoutes/MainRoutes.jsx
+++ app/src/components/MainRoutes/MainRoutes.jsx
@@ -6,6 +6,7 @@ import CadUsuarioPage from '../../pages/CadUsuarioPage/CadUsuarioPage'
 import LoginPage from '../../pages/LoginPage/LoginPage'
 import EditarPostPage from '../../pages/EditarPostPage/EditarPostPage'
 import ExcluirPostPage from '../../pages/ExcluirPostPage/ExcluirPostPage'
+import ContaUsuarioPage from '../../pages/ContaUsuarioPage/ContaUsuarioPage'
 import { useContext } from 'react'
 import { AppContext } from '../AppContext/AppContext'
 
@@ -30,8 +31,8 @@ const MainRoutes = () => {
 
 
                 {/* Rotas referentes ao usuario */}
-                <Route path="/usuarios/cadastro" element={<CadUsuarioPage />} />
-                <Route path="/usuarios/login" element={<LoginPage />} />
+                <Route path="/usuarios/cadastro" element={!tokenAuth ? <CadUsuarioPage />: <ContaUsuarioPage/>} />
+                <Route path="/usuarios/login" element={!tokenAuth ? <LoginPage />: <ContaUsuarioPage/>} />
 
                 {/* no home vai ter o get started com uma landing page, é pra ter uma pg pros posts */}
             </Routes>
