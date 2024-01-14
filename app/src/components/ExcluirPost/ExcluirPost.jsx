bb757f6ba420cebb610d30571d94b73d9722cfb2
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-14 14:25:27 -0300

    adicionando headers de autenticação na chamada a api

--- app/src/components/ExcluirPost/ExcluirPost.jsx
+++ app/src/components/ExcluirPost/ExcluirPost.jsx
@@ -1,44 +1,54 @@
 /* eslint-disable no-undef */
-import { Button } from '@chakra-ui/react'
+import { Button, Center } from '@chakra-ui/react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { api } from "../../services/api"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { getPostById } from '../../services/getPosts'
+import { AppContext } from '../AppContext/AppContext'
+
 
 
 const ExcluirPost = () => {
-    const  navigate =  useNavigate()
+    const {tokenAuth} =  useContext(AppContext)
+    const navigate = useNavigate()
     const { key } = useParams();
     const [id_post, setIdPost] = useState("")
-    useEffect(()=>{
-        const getPost = async (key) =>{
+    useEffect(() => {
+        const getPost = async (key) => {
             const data = await getPostById(key);
-            setIdPost( data[0].id_post)
+            setIdPost(data[0].id_post)
         }
         getPost(key)
     }, [])
 
-    
+
     const handleExcluirPostClick = () => {
         excluirPost(id_post)
         navigate("/postagens")
     };
 
     const excluirPost = (id_post) => {
-        api.delete(`/postagens/excluir/${id_post}`)
+        api.delete(`/postagens/excluir/${id_post}`, {
+            headers: {
+                'Authorization': `token ${tokenAuth}`
+            }
+        })
     }
 
 
     return (
         <>
-            <Button
-                        backgroundColor={"#303030"}
-                        color={"#fff"}
-                        size='lg'
-                        onClick={handleExcluirPostClick}
-                    >
+            <Center>
+                <Button
+                    backgroundColor={"#303030"}
+                    color={"#fff"}
+                    size='lg'
+                    onClick={handleExcluirPostClick}
+                >
                     Excluir
-            </Button>
+                </Button>
+            </Center>
+
         </>
     )
 }
