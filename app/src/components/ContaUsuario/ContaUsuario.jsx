59911298a151aa37699fd2ea782a8c746a03a084
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-17 14:26:34 -0300

    fazendo a importação dos elementos necessarios para deslogar

--- app/src/components/ContaUsuario/ContaUsuario.jsx
+++ app/src/components/ContaUsuario/ContaUsuario.jsx
@@ -1,7 +1,27 @@
-const ContaUsuario = () =>{
-    return(
+import { Button } from "@chakra-ui/react"
+import { useContext } from "react"
+import { AppContext } from "../AppContext/AppContext"
+import { deleteCookie } from "../../services/funcCookies"
+
+const ContaUsuario = () => {
+    const { tokenAuth, setTokenAuth } = useContext(AppContext)
+    const handleClickDeslogar = () => {
+        deleteCookie('token')
+        setTokenAuth("")
+    }
+
+
+    return (
         <>
             <h1>Informções da conta do usuario</h1>
+            <Button
+                backgroundColor={"#303030"}
+                color={"#fff"}
+                size='lg'
+                onClick={handleClickDeslogar}
+            >
+                Deslogar
+            </Button>
         </>
     )
 }
