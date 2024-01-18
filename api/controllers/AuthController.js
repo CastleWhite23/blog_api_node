71f5bf5285a90695b2536c8cf180327cb06e40a6
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-18 08:18:27 -0300

    retornando erros direito

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,17 +6,17 @@ class AuthController {
     async login(req, res) {
         const { username, senha } = req.body
 
-        if (!username || !senha) res.status(400).json({ error: "Preencha todos os campos" })
+        if (!username || !senha) return res.status(400).json({ error: "Preencha todos os campos" })
 
         const response = await usuarioModel.verificaConta(username, senha)
 
         if (response.message) {
-            return res.status(400).json(response.message)
+            return res.status(400).json({error: response.message})
         }
-        const realizarLogin = authModel.login(response.username)
+        const realizarLogin =  await authModel.login(response.username)
 
         if (!realizarLogin.auth) {
-            return res.status(400).json({ message: "login nao realizado" })
+            return res.status(400).json({ error:  "login nao realizado" })
         }
 
         return res.status(200).json({ token: realizarLogin.token, message: "login realizado com sucesso" })
