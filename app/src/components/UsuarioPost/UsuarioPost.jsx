fd02906613204fb80893b210746ed56af605fa5e
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-18 08:50:25 -0300

    passando o headers para autenticação

--- app/src/components/UsuarioPost/UsuarioPost.jsx
+++ app/src/components/UsuarioPost/UsuarioPost.jsx
@@ -1,9 +1,10 @@
 
 import { api } from "../../services/api";
 import { Spinner } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './style.css'
 import CardEditavel from "../CardEditavel/CardEditave";
+import { AppContext } from "../AppContext/AppContext";
 
 const Blogs = () => {
     //fazer requisição https para api
@@ -12,11 +13,16 @@ const Blogs = () => {
     //colocar uma condição para ter spinner de carregamento
     const [postData, setPostData] = useState([])
     const [allPostLoaded, setAllPostLoaded] = useState(false)
+    const {tokenAuth} = useContext(AppContext)
 
     useEffect(() => {
         const getAllPosts = async () => {
             try {
-                const allPost = await api.get('/postagens')
+                const allPost = await api.get('/usuario/posts', {
+                    headers: {
+                        "Authorization" : `token ${tokenAuth}`
+                    }
+                })
                 const allPostData = allPost.data
 
                 setPostData(allPostData)
